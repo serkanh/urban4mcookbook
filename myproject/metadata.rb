dba1a94c36607ff730327375bed705ae3330eece
Serkan Haytac <shaytac@gmail.com>
2014-03-18 09:40:01 -0400

    moved to top leve

---
+++ myproject/metadata.rb
@@ -0,0 +1,18 @@
+name             'myproject'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures myproject'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+#depends 'apt'
+depends 'build-essential'
+depends 'chef_handler'
+depends 'git'
+#depends 'dmg'
+#depends 'vim'
+depends 'runit'
+depends 'python'
+#depends 'yum'
+#depends 'yum-epel'
