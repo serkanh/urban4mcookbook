b7397f2c6c1c6f7134b277a7bfa59f29a53e65cf
Serkan Haytac <shaytac@gmail.com>
2014-03-18 12:56:21 -0400

    Recipe added to create urban4m user

--- myproject/recipes/default.rb
+++ myproject/recipes/default.rb
@@ -7,24 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#execute "apt-get-update-periodic" do
-#  command "apt-get update"
-#  ignore_failure true
-#  only_if do
-#    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
-#    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
-#  end
-#end
 
-#include_recipe 'apt'
 include_recipe 'build-essential'
 include_recipe 'chef_handler'
 include_recipe 'git'
 include_recipe 'dmg'
 include_recipe 'python'
-#include_recipe 'yum'
-#include_recipe 'yum-epel'
-#include_recipe 'vim'
 
 
 python_virtualenv "/home/urban4m/urban4menv" do
---
+++ myproject/recipes/urban4m.rb
@@ -0,0 +1,7 @@
+user "urban4m" do
+  shell "/bin/bash"
+  home "/home/urban4m"
+  comment "Urban4M"
+  supports :manage_home=>true
+  action :create
+end
