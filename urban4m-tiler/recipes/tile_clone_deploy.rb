62b44466ca9585b2cb3aeaaf3731d319738fb17b
Serkan Haytac <shaytac@gmail.com>
2014-03-20 12:16:11 -0400

    Removed unnecessary files

--- urban4m-tiler/recipes/tile_clone_deploy.rb
+++ urban4m-tiler/recipes/tile_clone_deploy.rb
@@ -3,26 +3,25 @@ include_recipe 'python'
 
 
 
-#run pull app on the server
+
 bash "run_pull"  do
   user "urban4m"
   code <<-EOH
-  bash /home/deploy/update.sh
+  bash /home/urban4m/deploy/update.sh
   EOH
 end
 
 #install application.
 python_pip "/home/urban4m/deploy/urbanpy" do
-  virtualenv "/home/venv-tiler"
+  virtualenv "/home/urban4m/venv-tiler"
   options '-e'
 end
 
-
-#run pull app on the server
+#restart smartmap
 bash "smartmap_restart"  do
   user "ubuntu"
   code <<-EOH
-  bash service smartmap_tiler restart
+  sudo service smartmap_tiler restart
   EOH
 end
 
@@ -30,31 +29,4 @@ end
 
 
 
-
-#create source directory to download application repo.
-#directory "/usr/local/urbanpy" do
-#  owner "urban4m"
-#  group "urban4m"
-#  mode 0755
-#  action :create
-#end
-
-
-#clone application to directory
-#git "/usr/local/urbanpy" do
-#  repository "http://serkan@git.urban4m.com/scm/urban/urbanpy.git"
-#  revision "master"
-#  action :sync
-#  user "urban4m"
-#end
-
-
-
-
-#python_virtualenv "/home/urban4m/urban4menv" do
-#  interpreter "python2.7"
-#  owner "urban4m"
-#  group "urban4m"
-#  action :create
-#end
 
--- urban4m-tiler/recipes/urban4m.rb
+++
@@ -1,8 +0,0 @@
-#create user and set home folder
-user "urban4m" do
-  shell "/bin/bash"
-  home "/home/urban4m"
-  comment "Urban4M"
-  supports :manage_home=>true
-  action :create
-end
