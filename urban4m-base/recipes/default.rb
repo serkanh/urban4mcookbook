f93ff7e1a3d7d842bf1620da612624b12ad35063
Serkan Haytac <shaytac@gmail.com>
2014-03-21 14:39:08 -0400

    Added base setup for urban4m servers

---
+++ urban4m-base/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: myproject
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe 'build-essential'
+include_recipe 'chef_handler'
+include_recipe 'git'
+include_recipe 'dmg'
+include_recipe 'python'
+
+
+
+
+
+
+
+
+
