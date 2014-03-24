d24183baec55bb1216ab9ca18603d66606ad552c
Serkan Haytac <shaytac@gmail.com>
2014-03-24 09:03:23 -0400

    Added urban4m-base cookbook added to provision base server for tiler and database with virtualenvs

--- urban4m-base/recipes/default.rb
+++ urban4m-base/recipes/default.rb
@@ -15,6 +15,54 @@ include_recipe 'dmg'
 include_recipe 'python'
 
 
+#Runs apt-get to update repos
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end
+
+
+package "update-notifier-common" do
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
+end
+
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+   File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+   File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
+
+#
+# Check env variable to determine whether
+# this is local vagrant env or aws server
+#
+
+package "curl"
+
+bash "install_pip"  do
+   user "urban4m"
+   code <<-EOH
+   sudo apt-get install curl
+   curl -L 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python'
+   EOH
+end
+
+
+#
+# Install Redis Server
+#
+bash "install-redis" do
+  user "urban4m"
+   code <<-EOH
+   sudo add-apt-repository -y ppa:rkwy/redis
+   sudo apt-get -y install redis
+   EOH
+end
+
 
 
 
